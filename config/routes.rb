5e4fb56c905043b523490286fade67b318a792fd
Oigen <ekonvissar@gmail.com>
2022-09-23 21:47:41 +0300

    Add a Statik Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'statik_pages/home'
+  get 'statik_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/statik_pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class StatikPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get statik_pages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get statik_pages_help_url
+    assert_response :success
+  end
+end
