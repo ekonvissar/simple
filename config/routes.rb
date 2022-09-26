30cbbff187da62570dcc58c5081917d15da1773b
Oigen <ekonvissar@gmail.com>
2022-09-26 22:21:16 +0300

    Add def

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'statik_pages#home'
   get 'statik_pages/help'
   get 'statik_pages/about'
+  get 'statik_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
